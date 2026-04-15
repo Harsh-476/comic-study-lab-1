5088ae48deb1b3df40617b57faa61186b411ad5f
unknown <aabhak626@gmail.com>
2026-04-15 22:34:24 +0530

    Updated font

--- src/page10.jsx
+++ src/page10.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 import "./page10.css";
 
@@ -19,23 +20,39 @@ const Projects = () => {
     <div className="projects-container">
       <div className="title">Creatives: Projects</div>
       <button className="logout" onClick={handleLogout}>LOG OUT</button>
+=======
+import "./CreativesProjects.css";
+
+const CreativesProjects = () => {
+  return (
+    <div className="container">
+      {/* Title */}
+      <div className="title">Creatives: Projects</div>
+
+      {/* Logout */}
+      <div className="logout">LOG OUT</div>
+>>>>>>> 8ecd37a (Updated font)
 
       {/* DOODLES */}
-      <img src={doodle} className="img curl-top-left" alt="doodle" />
-      <img src={doodle} className="img curl-right" alt="doodle" />
+      <img src="/images/doodle.png" alt="doodle" className="img curl-top-left" />
+      <img src="/images/doodle.png" alt="doodle" className="img curl-right" />
 
       {/* LEAVES */}
-      <img src={leaf} className="img leaf-left" alt="leaf" />
-      <img src={leaf} className="img leaf-right-top" alt="leaf" />
-      <img src={leaf} className="img leaf-right-mid" alt="leaf" />
+      <img src="/images/leaf.png" alt="leaf" className="img leaf-left" />
+      <img src="/images/leaf.png" alt="leaf" className="img leaf-right-top" />
+      <img src="/images/leaf.png" alt="leaf" className="img leaf-right-mid" />
 
       {/* STARS */}
-      <img src={star} className="img star-left" alt="star" />
-      <img src={star} className="img star-bottom" alt="star" />
-      <img src={star} className="img star-top1" alt="star" />
-      <img src={star} className="img star-top2" alt="star" />
+      <img src="/images/star.png" alt="star" className="img star-left" />
+      <img src="/images/star.png" alt="star" className="img star-bottom" />
+      <img src="/images/star.png" alt="star" className="img star-top1" />
+      <img src="/images/star.png" alt="star" className="img star-top2" />
     </div>
   );
 };
 
+<<<<<<< HEAD
 export default Projects;
+=======
+export default CreativesProjects;
+>>>>>>> 8ecd37a (Updated font)
